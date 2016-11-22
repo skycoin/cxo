db79c387f842755fa8a14fbdb379cc8347d877fe
Denis Korkhov <denis.korkhov@sqdron.com>
2016-11-22 21:27:31 +0300

    Refactoring

--- bbs/model.go
+++ bbs/model.go
@@ -7,22 +7,14 @@ import (
 type Board struct {
 	Name    string
 	Threads schema.HArray `type:"Thread"`
-	//Threads schema.HrefStatic `href:"[]Thread"`
 }
 
 type Thread struct {
 	Name  string
-	Board schema.HrefStatic `href:"[]Post"`
+	Posts schema.HArray `type:"Post"`
 }
 
 type Post struct {
 	Poster []byte
 	Text   string
 }
-
-func (b *Board) AddThread(thread *Thread) Board {
-	return Board{Name:b.Name,}
-}
-
-
-
