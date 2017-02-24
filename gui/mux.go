1dc801a1967d2ff49ce041b678014a1d184f3ecc
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-02-24 12:19:13 +0300

    move gui from nodeManager to new node package

--- gui/mux.go
+++ gui/mux.go
@@ -36,10 +36,9 @@ type branch struct {
 
 type muxBranches map[string]branch
 
-type IRouterApi interface{
+type IRouterApi interface {
 	Register(router *Router)
 }
-
 
 // NewRouter creates and returns pointer to a new router object
 func NewRouter() *Router {
