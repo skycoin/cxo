49e0a6a948cc3254aa57c43fa3f9b2b1933791b4
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-03-03 00:03:38 +0300

    clean up

--- data/db.go
+++ data/db.go
@@ -14,12 +14,14 @@ type DB struct {
 	data map[cipher.SHA256][]byte
 }
 
-type Statistic struct {
+type Stat struct {
 	Total  int `json:"total"`
 	Memory int `json:"memory"`
 }
 
-func (s Statistic) String() string {
+// String implemets fmt.Stringer interface and returns
+// human readable string
+func (s Stat) String() string {
 	return fmt.Sprintf("{total: %d, memory: %s}",
 		s.Total,
 		humanMemory(s.Memory))
@@ -48,69 +50,17 @@ func humanMemory(bytes int) string {
 		".") + ms
 }
 
-type QueryFunc func(key cipher.SHA256, data []byte) bool
-
-type IDataSource interface {
-	Save(value interface{}) cipher.SHA256
-	Update(value []byte) cipher.SHA256
-	Add(ds cipher.SHA256, value []byte) error
-	Has(ds cipher.SHA256) bool
-	Get(ds cipher.SHA256) ([]byte, bool)
-	Where(QueryFunc) []cipher.SHA256
-	Statistic() Statistic
-
-	Data() map[cipher.SHA256][]byte
-}
-
 func NewDB() *DB {
 	return &DB{
 		data: make(map[cipher.SHA256][]byte),
 	}
 }
 
-func createKey(data []byte) cipher.SHA256 {
-	return cipher.SumSHA256(data)
-}
-
-func (d *DB) Save(value interface{}) cipher.SHA256 {
-	return d.Update(encoder.Serialize(value))
-}
-
-func (d *DB) Update(data []byte) cipher.SHA256 {
-	key := createKey(data)
-	if key == (cipher.SHA256{}) || data == nil {
-		panic("Invalid key")
-	}
-	d.Lock()
-	d.data[key] = data
-	d.Unlock()
-	return key
-}
-
-func (d *DB) Add(key cipher.SHA256, value []byte) (err error) {
-	if key == (cipher.SHA256{}) || value == nil {
-		panic("Invalid key")
-	}
-
-	d.Lock()
-	defer d.Unlock()
-
-	if d.has(key) {
-		return fmt.Errorf("key already present: %v", key)
-	}
-	d.data[key] = value
-	return
-}
-
-func (d *DB) has(key cipher.SHA256) (ok bool) {
-	_, ok = d.data[key]
-	return
-}
-
-func (d *DB) Has(key cipher.SHA256) bool {
+func (d *DB) Has(key cipher.SHA256) (ok bool) {
 	d.RLock()
 	defer d.RUnlock()
-	return d.has(key)
+	_, ok = d.data[key]
+	return
 }
 
 func (d *DB) Get(key cipher.SHA256) (v []byte, ok bool) {
@@ -120,29 +70,18 @@ func (d *DB) Get(key cipher.SHA256) (v []byte, ok bool) {
 	return
 }
 
-func (d *DB) Where(q QueryFunc) []cipher.SHA256 {
-	result := []cipher.SHA256{}
-	d.RLock()
-	defer d.RUnlock()
-	for key, value := range d.data {
-		if q(key, value) {
-			result = append(result, key)
-		}
-	}
-	return result
+func (d *DB) Set(key cipher.SHA256, data []byte) {
+	d.Lock()
+	defer d.Unlock()
+	d.data[key] = data
 }
 
-func (d *DB) Statistic() (s Statistic) {
+func (d *DB) Stat() (s Stat) {
 	d.RLock()
 	d.RUnlock()
 	s.Total = len(d.data)
 	for _, v := range d.data {
-		s.Memory += len(v) // + len(cipher.SHA256) ?
+		s.Memory += len(v)
 	}
 	return
-}
-
-// it's unsafe to use it asyncronously
-func (d *DB) Data() map[cipher.SHA256][]byte {
-	return d.data
 }
