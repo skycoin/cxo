1c15b8e43719c6ae2c0e069159ad478410c8815e
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2017-03-04 03:15:05 +1300

    Shows old skyobjects

---
+++ skyobjects_old/href.object.go
@@ -0,0 +1,51 @@
+package skyobject
+
+import (
+	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/cipher/encoder"
+)
+
+type HashObject Href
+
+func NewObject(value interface{}) HashObject {
+	res := HashObject{value: value}
+	sm := ReadSchema(value)
+	res.rtype = encoder.Serialize(sm)
+	return res
+}
+
+func (h *HashObject) SetData(tp []byte, data []byte) {
+	h.rdata = data
+}
+
+func (h *HashObject) Type() cipher.SHA256 {
+	return cipher.SumSHA256(h.rtype)
+}
+
+func (h *HashObject) save(c ISkyObjects) Href {
+	typeKey := c.SaveData(_schemaType, h.rtype)
+	h.rdata = encoder.Serialize(h.value)
+	h.Ref = c.SaveData(typeKey, h.rdata)
+	return Href(*h)
+}
+
+func (h *HashObject) References(c ISkyObjects) RefInfoMap {
+	result := RefInfoMap{}
+	objSchema := Schema{}
+	if h.rtype != nil {
+		encoder.DeserializeRaw(h.rtype, &objSchema)
+		for _, f := range objSchema.Fields {
+			if c.ValidateHashType(f.Type) {
+				var ref Href
+				encoder.DeserializeField(h.rdata, objSchema.Fields, f.Name, &ref.Ref)
+				mergeRefs(result, ref.References(c))
+			}
+		}
+	}
+	//fmt.Println("Object References ", result)
+	return result
+}
+
+func (h *HashObject) String(c ISkyObjects) string {
+	return ""
+}
