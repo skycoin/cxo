c415ed27ce54e7586f4e0675b1d47a76c58fd9d5
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-04-11 22:48:38 +0300

    node/gnet tests for NewPool

--- node/gnet/pool_test.go
+++ node/gnet/pool_test.go
@@ -3,14 +3,12 @@ package gnet
 import (
 	"io/ioutil"
 	"testing"
+	"time"
 
 	"github.com/skycoin/cxo/node/log"
 )
 
-func testConfig() (c Config) {
-	c = testConfigName("")
-	return
-}
+func testConfig() Config { return testConfigName("") }
 
 func testConfigName(name string) (c Config) {
 	c = NewConfig()
@@ -26,11 +24,115 @@ func testConfigName(name string) (c Config) {
 	return
 }
 
+// a limited by time task
+
+func timeLimit(tm time.Duration, fatal func()) (breakChan chan struct{}) {
+	breakChan = make(chan struct{})
+	go func() {
+		select {
+		case <-breakChan:
+		case <-time.After(tm):
+			fatal()
+		}
+	}()
+	return
+}
+
 // Any is type for tests
 type Any struct{ Value string }
 
 func TestNewPool(t *testing.T) {
-	// TODO: high priority
+	t.Run("configs", func(t *testing.T) {
+		t.Run("defaults", func(t *testing.T) {
+			p := NewPool(Config{
+				MaxConnections:  -1,
+				MaxMessageSize:  -1,
+				DialTimeout:     -1,
+				ReadTimeout:     -1,
+				WriteTimeout:    -1,
+				ReadBufferSize:  -1,
+				WriteBufferSize: -1,
+				ReadQueueSize:   -1,
+				WriteQueueSize:  -1,
+				PingInterval:    -1,
+			})
+			if p.conf.MaxConnections != MaxConnections {
+				t.Error("negative MaxConnections wasn't set to default value:",
+					p.conf.MaxConnections)
+			}
+			if p.conf.MaxMessageSize != MaxMessageSize {
+				t.Error("negative MaxMessageSize wasn't set to default value:",
+					p.conf.MaxMessageSize)
+			}
+			if p.conf.DialTimeout != DialTimeout {
+				t.Error("negative DialTimeout wasn't set to default value:",
+					p.conf.DialTimeout)
+			}
+			if p.conf.ReadTimeout != ReadTimeout {
+				t.Error("negative ReadTimeout wasn't set to default value:",
+					p.conf.ReadTimeout)
+			}
+			if p.conf.WriteTimeout != WriteTimeout {
+				t.Error("negative WriteTimeout wasn't set to default value:",
+					p.conf.WriteTimeout)
+			}
+			if p.conf.ReadBufferSize != ReadBufferSize {
+				t.Error("negative ReadBufferSize wasn't set to default value:",
+					p.conf.ReadBufferSize)
+			}
+			if p.conf.WriteBufferSize != WriteBufferSize {
+				t.Error("negative WriteBufferSize wasn't set to default value:",
+					p.conf.WriteBufferSize)
+			}
+			if p.conf.ReadQueueSize != ReadQueueSize {
+				t.Error("negative ReadQueueSize wasn't set to default value:",
+					p.conf.ReadQueueSize)
+			}
+			if p.conf.WriteQueueSize != WriteQueueSize {
+				t.Error("negative WriteQueueSize wasn't set to default value:",
+					p.conf.WriteQueueSize)
+			}
+			if p.conf.PingInterval != PingInterval {
+				t.Error("negative PingInterval wasn't set to default value:",
+					p.conf.PingInterval)
+			}
+		})
+		t.Run("min timeouts", func(t *testing.T) {
+			p := NewPool(Config{
+				ReadTimeout:  minTimeout - 1*time.Nanosecond,
+				WriteTimeout: minTimeout - 1*time.Nanosecond,
+				PingInterval: minPingInterval - 1*time.Nanosecond,
+			})
+			if p.conf.ReadTimeout != minTimeout {
+				t.Error("ReadTimeout is lesser then allowed minimum")
+			}
+			if p.conf.WriteTimeout != minTimeout {
+				t.Error("WriteTimeout is lesser then allowed minimum")
+			}
+			if p.conf.PingInterval != minPingInterval {
+				t.Error("PingInterval is lesser then allowed minimum")
+			}
+		})
+		t.Run("logger", func(t *testing.T) {
+			p := NewPool(Config{})
+			if p.Logger == nil {
+				t.Error("logger of pool wasn't created")
+			}
+			l := log.NewLogger("prefix", false)
+			p = NewPool(Config{Logger: l})
+			if p.Logger != l {
+				t.Error("logger of pool was replaced")
+			}
+		})
+	})
+	t.Run("immediate close", func(t *testing.T) {
+		p := NewPool(Config{})
+		done := timeLimit(50*time.Millisecond, func() {
+			t.Fatal("closing of pool is too slow")
+		})
+		p.Close()
+		close(done)
+	})
 }
 
 func TestPool_Listen(t *testing.T) {
