694e61c3cb91cb762c63061f9726c5ac3a71beb4
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-04-22 10:30:40 +0300

    bss/ common draft

--- bbs/bbs.go
+++ bbs/bbs.go
@@ -1,8 +1,32 @@
 package bbs
 
 import (
+	"github.com/skycoin/skycoin/src/cipher"
+
 	"github.com/skycoin/cxo/data"
 	"github.com/skycoin/cxo/skyobject"
 )
 
-//
+// A Board
+type Board struct {
+	Header  string
+	Threads skyobject.References
+}
+
+// A Thread
+type Thread struct {
+	Header string
+	Posts  skyobject.References
+}
+
+// A Post
+type Post struct {
+	Header string
+	Body   string
+}
+
+// A BBS instance that used by an owner of a feed
+type BBS struct {
+	owner cipher.SecKey
+	so    *skyobject.Container
+}
