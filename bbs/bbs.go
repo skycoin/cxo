3296f74cba0b5fe475174ece991406b9c273e9b1
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-04-22 16:02:02 +0300

    bbs/ keep

--- bbs/bbs.go
+++ bbs/bbs.go
@@ -28,5 +28,33 @@ type Post struct {
 // A BBS instance that used by an owner of a feed
 type BBS struct {
 	owner cipher.SecKey
+	feed  cipher.PubKey
 	so    *skyobject.Container
+}
+
+// NewBBS creates BBS of given owner to wrok with given feed
+func NewBBS(owner cipher.SecKey, feed cipher.PubKey,
+	so *skyobject.Container) (b *BBS) {
+
+	b = new(BBS)
+
+	if so == nil {
+		panic("nil container")
+	}
+	if owner == (cipher.SecKey{}) {
+		panic("empty owner")
+	}
+	if feed == (cipher.PubKey{}) {
+		panic("empty feed")
+	}
+
+	b.owner = owner
+	b.feed = feed
+	b.so = so
+
+	return
+}
+
+func (b *BBS) name() {
+
 }
