e794c1f15d3ff96a45c9b245151f7e1175f9852d
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-04-30 21:59:51 +0300

    node/ rpc cient

--- node/rpc_client.go
+++ node/rpc_client.go
@@ -2,8 +2,81 @@ package node
 
 import (
 	"net/rpc"
+
+	"github.com/skycoin/cxo/data"
+	"github.com/skycoin/skycoin/src/cipher"
 )
 
 type RPCClient struct {
 	c *rpc.Client
+}
+
+func (r *RPCClient) Want(feed cipher.PubKey) (list []cipher.SHA256, err error) {
+	err = r.c.Call("cxo.Want", feed, &list)
+	return
+}
+
+func (r *RPCClient) Got(feed cipher.PubKey) (list *[]cipher.SHA256, err error) {
+	err = r.c.Call("cxo.Got", feed, &list)
+	return
+}
+
+func (r *RPCClient) AddFeed(feed cipher.PubKey) (ok bool, err error) {
+	err = r.c.Call("cxo.AddFeed", feed, &ok)
+	return
+}
+
+func (r *RPCClient) DelFeed(feed cipher.PubKey) (ok bool, err error) {
+	err = r.c.Call("cxo.DelFeed", feed, &ok)
+	return
+}
+
+func (r *RPCClient) Feeds() (list []cipher.PubKey, err error) {
+	err = r.c.Call("cxo.Feeds", struct{}{}, &list)
+	return
+}
+
+func (r *RPCClient) Stat() (stat data.Stat, err error) {
+	r.c.Call("cxo.Stat", struct{}{}, &stat)
+	return
+}
+
+func (r *RPCClient) Connections() (list []string, err error) {
+	err = r.c.Call("cxo.Connections", struct{}{}, &list)
+	return
+}
+
+func (r *RPCClient) IncomingConnections() (list []string, err error) {
+	err = r.c.Call("cxo.IncomingConnections", struct{}{}, &list)
+	return
+}
+
+func (r *RPCClient) OutgoingConnections() (list []string, err error) {
+	err = r.c.Call("cxo.OutgoingConnections", struct{}{}, &list)
+	return
+}
+
+func (r *RPCClient) Connect(address string) (err error) {
+	err = r.c.Call("cxo.Connect", address, &struct{}{})
+	return
+}
+
+func (r *RPCClient) Disconnect(address string) (err error) {
+	err = r.c.Call("cxo.Disconnect", address, &struct{}{})
+	return
+}
+
+func (r *RPCClient) ListeningAddress() (address string, err error) {
+	err = r.c.Call("cxo.ListeningAddress", struct{}{}, &address)
+	return
+}
+
+func (r *RPCClient) Tree(feed cipher.PubKey) (tree []byte, err error) {
+	err = r.c.Call("cxo.Tree", feed, &tree)
+	return
+}
+
+func (r *RPCClient) Terminate() (err error) {
+	err = r.c.Call("cxo.Terminate", struct{}{}, &struct{}{})
+	return
 }
