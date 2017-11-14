b167425b99279c738e7b593c8e56edfb5d1b449d
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-11-14 16:36:33 +0300

    skyobject/registry refactoring (mittelspiel/14) rewrite and improve Schema.Size test

--- skyobject/registry/schema_test.go
+++ skyobject/registry/schema_test.go
@@ -532,124 +532,42 @@ func TestSchema_Encode(t *testing.T) {
 func TestSchema_Size(t *testing.T) {
 	// Size(p []byte) (n int, err error)
 
-	r := testSchemaRegistry()
+	var (
+		reg = testRegistry()
 
-	sc, err := r.schemaByName("test.TestStruct")
-	if err != nil {
-		t.Fatal(err)
-	}
+		s   Schema
+		err error
+	)
 
-	ts := TestStruct{
-		TestInt8:  0,
-		TestInt16: 1,
-		TestInt32: 2,
-		TestInt64: 3,
+	for _, tt := range testTypes() {
 
-		TestUint8:  4,
-		TestUint16: 5,
-		TestUint32: 6,
-		TestUint64: 7,
+		t.Log(tt.Name)
 
-		TestInt8Array:  [3]TestInt8{8, 9, 10},
-		TestInt16Array: [3]TestInt16{11, 12, 13},
-		TestInt32Array: [3]TestInt32{14, 15, 16},
-		TestInt64Array: [3]TestInt64{17, 18, 19},
-
-		TestUint8Array:  [3]TestUint8{20, 21, 22},
-		TestUint16Array: [3]TestUint16{23, 24, 25},
-		TestUint32Array: [3]TestUint32{26, 27, 28},
-		TestUint64Array: [3]TestUint64{29, 30, 31},
-
-		TestInt8Slice:  []TestInt8{32, 33, 34, 35},
-		TestInt16Slice: []TestInt16{36, 37, 38, 39},
-		TestInt32Slice: []TestInt32{40, 41, 42, 43},
-		TestInt64Slice: []TestInt64{44, 45, 46, 47},
-
-		TestUint8Slice:  []TestUint8{48, 49, 50, 51},
-		TestUint16Slice: []TestUint16{52, 53, 54, 55},
-		TestUint32Slice: []TestUint32{56, 57, 58, 59},
-		TestUint64Slice: []TestUint64{60, 61, 62, 63},
-
-		TestString:      "64",
-		TestSmallStruct: TestSmallStruct{},
-
-		TestNumberStructArray: [3]TestNumberStruct{{65}, {66}, {67}},
-		TestNumberStructSlice: []TestNumberStruct{{68}, {69}, {70}, {71}},
-
-		Ref:     Ref{},
-		Refs:    Refs{},
-		Dynamic: Dynamic{},
-
-		// not embedded fields
-
-		FieldTestInt8:  72,
-		FieldTestInt16: 73,
-		FieldTestInt32: 74,
-		FieldTestInt64: 75,
-
-		FieldTestUint8:  76,
-		FieldTestUint16: 77,
-		FieldTestUint32: 78,
-		FieldTestUint64: 79,
-
-		FieldTestInt8Array:  [3]TestInt8{80, 81, 82},
-		FieldTestInt16Array: [3]TestInt16{83, 84, 85},
-		FieldTestInt32Array: [3]TestInt32{86, 87, 88},
-		FieldTestInt64Array: [3]TestInt64{89, 90, 91},
-
-		FieldTestUint8Array:  [3]TestUint8{92, 93, 94},
-		FieldTestUint16Array: [3]TestUint16{95, 96, 97},
-		FieldTestUint32Array: [3]TestUint32{98, 99, 100},
-		FieldTestUint64Array: [3]TestUint64{101, 102, 103},
-
-		FieldTestInt8Slice:  []TestInt8{104, 105, 106, 107},
-		FieldTestInt16Slice: []TestInt16{108, 109, 110, 111},
-		FieldTestInt32Slice: []TestInt32{112, 113, 114, 115},
-		FieldTestInt64Slice: []TestInt64{116, 117, 118, 119},
-
-		FieldTestUint8Slice:  []TestUint8{120, 121, 122, 123},
-		FieldTestUint16Slice: []TestUint16{124, 125, 126, 127},
-		FieldTestUint32Slice: []TestUint32{128, 129, 130, 131},
-		FieldTestUint64Slice: []TestUint64{132, 133, 134, 135},
-
-		FieldTestString:      "136",
-		FieldTestSmallStruct: TestSmallStruct{},
-
-		FieldTestNumberStructArray: TestNumberStructArray{
-			{137},
-			{138},
-			{139},
-		},
-		FieldTestNumberStructSlice: TestNumberStructSlice{},
-
-		FieldRef:     Ref{},
-		FieldRefs:    Refs{},
-		FieldDynamic: Dynamic{},
-	}
-
-	var p = encoder.Serialize(ts)
-	//var i int
-
-	if n, err := sc.Size(p); err != nil {
-		t.Error(err)
-	} else if n != len(p) {
-		t.Error("wriong size")
-	}
-
-	// empty
-	var tse TestStruct
-	ep := encoder.Serialize(tse)
-	if n, err := sc.Size(ep); err != nil {
-		t.Error(err)
-	} else if n != len(ep) {
-		t.Error("wrong size")
-	}
-
-	// truncate
-	for i := len(p) - 1; i >= 0; i-- {
-		if _, err := sc.Size(p[:i]); err == nil {
-			t.Error("missing error")
+		if s, err = reg.SchemaByName(tt.Name); err != nil {
+			t.Error(err)
+			continue
 		}
+
+		var data = encoder.Serialize(tt.Val)
+
+		if ss, ds := s.Size(data), len(data); ss != ds {
+			t.Error("wrong struct Size: want %d, got %d", ds, ss)
+		}
+
+		var rv = reflect.Indirect(reflect.ValueOf(tt.Val))
+
+		for i, fl := range s.Fields() {
+
+			var rf = rv.Field(i)
+
+			data = encoder.Serialize(rf.Interface())
+
+			if fss, ds := fl.Schema().Size(data); fss != ds {
+				t.Errorf("wrong Schema Size: want %d, got %d", ds, fss)
+			}
+
+		}
+
 	}
 
 }
