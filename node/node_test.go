7febd00fb01293b9e158ae64798b2f972f08e78b
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-12-19 21:03:15 +0300

    node/ add test for Publish method

--- node/node_test.go
+++ node/node_test.go
@@ -2,10 +2,12 @@ package node
 
 import (
 	"testing"
+	"time"
 
 	"github.com/skycoin/skycoin/src/cipher"
 
 	"github.com/skycoin/cxo/skyobject"
+	"github.com/skycoin/cxo/skyobject/registry"
 )
 
 func getTestConfigNotListen(prefix string) (c *Config) {
@@ -112,7 +114,130 @@ func TestNode_Container(t *testing.T) {
 func TestNode_Publish(t *testing.T) {
 	// (r *registry.Root)
 
-	//
+	// broadcast to all subscribers
+
+	// create listener (server, generator of root objects)
+
+	var (
+		ln = getTestNode("server")
+
+		gr1 = make(chan *registry.Root, 1) // sc1
+		gr2 = make(chan *registry.Root, 1) // sc2
+
+		gn1 = make(chan *registry.Root, 1) // should not receive (uc1)
+
+		sc1 = getTestConfigNotListen("sc1") // subscriber
+		sc2 = getTestConfigNotListen("sc2") // subscriber
+
+		uc1 = getTestConfigNotListen("uc1") // not subscribed
+	)
+
+	defer ln.Close()
+
+	// handle received roots
+
+	sc1.OnRootReceived = func(_ *Conn, r *registry.Root) (_ error) {
+		gr1 <- r
+		return
+	}
+	sc2.OnRootReceived = func(_ *Conn, r *registry.Root) (_ error) {
+		gr2 <- r
+		return
+	}
+	uc1.OnRootReceived = func(_ *Conn, r *registry.Root) (_ error) {
+		gn1 <- r
+		return
+	}
+
+	// create clients (subscribed, subscribed, and not subscribed)
+
+	var (
+		sn1, sn2, un1 *Node
+		err           error
+	)
+
+	if sn1, err = NewNode(sc1); err != nil {
+		t.Fatal(err)
+	}
+	defer sn1.Close()
+
+	if sn2, err = NewNode(sc2); err != nil {
+		t.Fatal(err)
+	}
+	defer sn2.Close()
+
+	if un1, err = NewNode(uc1); err != nil {
+		t.Fatal(err)
+	}
+	defer un1.Close()
+
+	// feed and owner
+
+	var pk, sk = cipher.GenerateKeyPair()
+
+	if err = ln.Share(pk); err != nil {
+		t.Fatal(err)
+	}
+
+	// conenct to the listener (and subscribe / not susbcribe)
+
+	if c, err := sn1.TCP().Connect(ln.TCP().Address()); err != nil {
+		t.Fatal(err)
+	} else if err = c.Subscribe(pk); err != nil {
+		t.Fatal(err)
+	}
+
+	if c, err := sn2.TCP().Connect(ln.TCP().Address()); err != nil {
+		t.Fatal(err)
+	} else if err = c.Subscribe(pk); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = un1.TCP().Connect(ln.TCP().Address()); err != nil {
+		t.Fatal(err)
+	}
+
+	// generate Root
+
+	var up *skyobject.Unpack
+	if up, err = ln.Container().Unpack(sk, getTestRegistry()); err != nil {
+		t.Fatal(err)
+	}
+
+	var r = new(registry.Root)
+
+	r.Pub = pk
+	r.Nonce = 9012
+
+	// save blank Root (it's enough for this test)
+
+	if err = ln.Container().Save(up, r); err != nil {
+		t.Error(err)
+	}
+
+	// publish
+
+	ln.Publish(r)
+
+	// check out channels
+
+	select {
+	case <-gr1:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("not received")
+	}
+
+	select {
+	case <-gr2:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("not received")
+	}
+
+	select {
+	case <-gn1:
+		t.Fatal("received")
+	case <-time.After(100 * time.Millisecond):
+	}
 
 }
 
